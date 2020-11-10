a3d48738ea9685690822f613e553c00d028f9be9
Wonderful Prince <wonderp087@gmail.com>
2020-11-11 03:42:03 +0530

    Board flip implemented

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,19 +5,42 @@ import './Board.scss';
 
 class Board extends React.Component {
     static contextType = GameContext;
-    render() {
+
+    constructor(props) {
+        super(props);
+        this.setBoard = this.setBoard.bind(this);
+
+        this.state = {
+            isWhite: false
+        }
+    }
+
+    setBoard() {
         let board = [];
         for(let i=1;i<=8;i++){
             let row = [];
             for(let j=1;j<=8;j++) {
-                let name = `${String.fromCharCode(96+i)}${(117-j) % 9}`;
+                let rank, file;
+                if(this.state.isWhite){
+                    file = 96 + i;
+                    rank = (117 - j) % 9;
+                } else {
+                    file = 105-i;
+                    rank = j;
+                }
+                let name = `${String.fromCharCode(file)}${rank}`;
                 row.push(<Square key={name} name={name} color={( ((i%2 !== 0) && (j%2 !== 0) || (i%2 === 0) && (j%2 === 0)) ? 'light' : 'dark')} coord={`${i}${j}`}/>);
             }
             board.push(row);
         }
+        return board;
+    }
+
+    render() {
+        
         return (
             <section className="Board-wrapper">
-                {board}
+                {this.setBoard()}
             </section>
         )
     }
