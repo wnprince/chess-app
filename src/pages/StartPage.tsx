3a487a3a320fa9ce59b4a04e66b127abb1c4e6b2
Wonderful Prince <wonderp087@gmail.com>
2020-12-21 20:45:18 +0530

    StartPage Complete

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const StartPage: React.FC = () => {
     return (
-        <div className="h-screen w-screen bg-main">
-            <div className="h-1/4 w-1/4 bg-secondary rounded-lg ">
-                Hello
+        <div className="h-screen w-screen flex items-center justify-center bg-main">
+            <div className="h-1/3 w-5/6 md:w-3/4 lg:w-1/2 xl:w-1/3 flex flex-col justify-start items-center bg-secondary rounded-lg ">
+                <div className="text-white font-bold text-3xl my-5">Choose One</div>
+                <div className="flex my-3">
+                    <div className="bg-main mx-3 h-16 w-16 rounded-lg">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Chess_tile_pl.svg"  height="64" width="64" alt=""/>
+                    </div>
+                    <div className="bg-main mx-3 h-16 w-16 rounded-lg">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/6/63/Chess_put45.svg" height="64" width="64" alt=""/>
+                    </div>
+                    <div className="bg-main mx-3 h-16 w-16 rounded-lg">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Chess_pdt26.svg" height="64" width="64" alt=""/>
+                    </div>
+                </div>
+                <button className="btn-start m-5">Start Game</button>
             </div>
         </div>
     )
